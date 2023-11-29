867771659a374fc8667f763c4c3bac0aa11273ac
vrushankportkey <134934501+vrushankportkey@users.noreply.github.com>
2023-11-29 16:15:26 +0530

    Create website_logs_python.py

---
+++ examples/website_logs_python.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="OPENAI_API_KEY",
+    base_url="https://api.portkey.ai/v1/proxy", ## Point to Portkey's gateway URL
+    default_headers= {
+        "x-portkey-api-key": "PORTKEY_API_KEY",
+        "x-portkey-mode": "proxy openai",
+        "Content-Type": "application/json"
+    }
+)
+
+chat_complete = client.chat.completions.create(
+    model="gpt-4",
+    messages=[{"role": "user", "content": "What's a Fractal?"}],
+)
+
+print(chat_complete.choices[0].message.content)
